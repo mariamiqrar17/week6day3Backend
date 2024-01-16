964be2658efb4dc51197492b3f281431b5b7449f
mariamiqrar17 <mariam_iqrar_intern@netixsol.com>
2024-01-16 14:25:26 +0500

    change

---
+++ pages/settings.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+
+export default function Home() {
+  return <Layout>
+    {/* <div className="text-blue-900 font-bold text-2xl flex justify-between">
+        Settings
+    </div> */}
+    <h1>Settings</h1>
+  </Layout>
+}
