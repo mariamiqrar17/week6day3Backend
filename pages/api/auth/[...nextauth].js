4bf563e379d8077e9cbd07e9ed22a566bee9d494
mariamiqrar17 <mariam_iqrar_intern@netixsol.com>
2024-01-19 12:21:55 +0500

    change

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['mariamiqrar2@gmail.com'];
+const adminEmails = [''];
 
 export const authOptions = {
   secret: process.env.SECRET,
